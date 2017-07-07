a9091f1477dee409fe4fca87045d085242faa967
Jason Ghiraldini <jason.ghiraldini@gmail.com>
2017-07-07 13:46:11 -0700

    Add connection to DB

--- userLogin.php
+++ userLogin.php
@@ -1,24 +1,33 @@
 <?php
-	  $passwordVar = $_POST['UserPassword'];
-	  
-      if(!($stmt = $mysqli->prepare("
-      SELECT UserPassword
-      FROM Users
-      WHERE (Users.UserPassword = '$passwordVar')" ))){
-        echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
-      }
-	  
-      if(!$stmt->execute()){
-        echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
-      }
-	  
-      if(!$stmt->bind_result($UserPassword)){
-        echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
-      }
-	  
-      while($stmt->fetch()){
-        echo "<tr>\n<td>\n" . $UserPassword .  "\n</td>\n</tr>";
-      }
 
-      $stmt->close();
+	ini_set('display_errors', 'On');
+	$mysqli = new mysqli("oniddb.cws.oregonstate.edu","ghiraldj-db","v1bptepGowZ4t1OE","ghiraldj-db");
+
+	if(!$mysqli || $mysqli->connect_errno){
+		echo "Connection error " . $mysqli->connect_errno . " " . 
+		 $mysqli->connect_error;
+	}
+
+	$passwordVar = $_POST['UserPassword'];
+
+	if(!($stmt = $mysqli->prepare("
+	SELECT UserPassword
+	FROM Users
+	WHERE (Users.UserPassword = '$passwordVar')" ))){
+	echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+	}
+
+	if(!$stmt->execute()){
+	echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+	}
+
+	if(!$stmt->bind_result($UserPassword)){
+	echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+	}
+
+	while($stmt->fetch()){
+	echo "<tr>\n<td>\n" . $UserPassword .  "\n</td>\n</tr>";
+	}
+
+	$stmt->close();
 ?>
