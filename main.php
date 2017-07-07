43a6c4fc16c4463125adbc71ed5da4edb92bcb3d
Jason Ghiraldini <jason.ghiraldini@gmail.com>
2017-07-07 08:43:53 -0700

    Updated drop downs
    Drop down no longer span the entire form

--- main.php
+++ main.php
@@ -31,7 +31,7 @@
 			
 				<div class="form-group row">
 					<label class="col-md-1" for="text">Migraine Intensity:</label>
-					<select class="form-control col-md-6" name="MigraineIntensity" required="">
+					<select class="selectpicker" name="MigraineIntensity" required="">
 						<option>1</option>
 						<option>2</option>
 						<option>3</option>
@@ -53,7 +53,7 @@
 				
 				<div class="form-group row">
 					<label class="col-md-1" for="text">Food and Drink:</label>
-					<select class="form-control col-md-6" name="FoodTriggerItem" required="">
+					<select class="selectpicker" name="FoodTriggerItem" required="">
 						<option>Chocolate</option>
 						<option>Alcohol</option>
 						<option>Cheese</option>
@@ -68,7 +68,7 @@
 
 				<div class="form-group row">
 					<label class="col-md-1" for="text">Sensory:</label>
-					<select class="form-control col-md-6" name="SensoryTriggerValue" required="">
+					<select class="selectpicker" name="SensoryTriggerValue" required="">
 						<option>Exposed to bright light</option>
 						<option>Exposed to loud sounds</option>
 						<option>Exposed to strong smells</option>
@@ -81,7 +81,7 @@
 
 				<div class="form-group row">
 					<label class="col-md-1" for="text">Water Intake:</label>
-					<select class="form-control col-md-6" name="WaterIntakeTriggerValue" required="">
+					<select class="selectpicker" name="WaterIntakeTriggerValue" required="">
 						<option>Had below 0.5 liters  of water</option>
 						<option>Had between  0.5 - 1 liters of water</option>
 						<option>Had between  1 - 1.5 liters of water</option>
@@ -93,7 +93,7 @@
 
 				<div class="form-group row">
 					<label class="col-md-1" for="text">Stress:</label>
-					<select class="form-control col-md-6" name="StressTriggerValue" required="">
+					<select class="selectpicker" name="StressTriggerValue" required="">
 						<option>Not stressed</option>
 						<option>Slightly stressed</option>
 						<option>Moderately stressed</option>
@@ -103,7 +103,7 @@
 
 				<div class="form-group row">
 					<label class="col-md-1" for="text">Physical Activity:</label>
-					<select class="form-control col-md-6" name="PhysicalActivityTriggerValue" required="">
+					<select class="selectpicker" name="PhysicalActivityTriggerValue" required="">
 						<option>Extremely inactive</option>
 						<option>Moderately inactive</option>
 						<option>Neutral</option>
@@ -115,7 +115,7 @@
 				
 				<div class="form-group row">
 					<label class="col-md-1" for="text">Sleep:</label>
-					<select class="form-control" name="SleepTriggerValue" required="">
+					<select class="selectpicker" name="SleepTriggerValue" required="">
 						<option>Did not sleep</option>
 						<option>Between 1 - 3  hours of sleep</option>
 						<option>Between 4 - 6 hours  hours of sleep</option>
