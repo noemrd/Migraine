cd9a04ed646cf2ca8b561d527c235257c9b6fe46
Jason Ghiraldini <jason.ghiraldini@gmail.com>
2017-07-07 14:44:16 -0700

    Testing validation of password and screen name

--- userLogin.php
+++ userLogin.php
@@ -24,7 +24,7 @@
 	if(!$stmt->bind_result($UserFirstName, $UserLastName, $UserScreenName, $UserPassword)){
 		echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
 	}
-
+	
 	while($stmt->fetch()){
 		echo "User First Name: " . $UserFirstName . PHP_EOL;
 		echo "User Last Name: " . $UserLastName . PHP_EOL;
@@ -32,5 +32,12 @@
 		echo "User Password: " . $UserPassword . PHP_EOL;
 	}
 
+	if ($UserPassword == ""){
+		echo "Incorrect Password";
+	}
+	if ($UserScreenName == ""){
+		echo "Screen Name does not exist";
+	}
+	
 	$stmt->close();
 ?>
