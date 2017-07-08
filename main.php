449fe6a6bfc1123f66f916219baac1c7e71b0dd9
Jason Ghiraldini <jason.ghiraldini@gmail.com>
2017-07-07 20:17:44 -0700

    Testing pushing form data to DB

--- main.php
+++ main.php
@@ -134,7 +134,8 @@
 
 				<div class="buttonAlign">
 					<button type="button"" class="buttonStyle" id="migraineDataCancel" onclick="window.location='landing.html'">Cancel</button>
-					<button type="submit" class="buttonStyle" id="migraineDataSubmit" value="Add Migraine Data" onclick="window.location='home.html'">Submit</button>
+					<!--<button type="submit" class="buttonStyle" id="migraineDataSubmit" value="Add Migraine Data" onclick="window.location='home.html'">Submit</button>-->
+					<button type="submit" class="buttonStyle" id="migraineDataSubmit" value="Add Migraine Data">Submit</button>
 				</div>
 			</form>
 		</div>
