1e570c80967727522f90ee1cd940e3e7ddda5ae1
AL2016 <lamaa@oregonstate.edu>
2017-07-10 04:08:52 -0400

    submit completion landing page

--- main.php
+++ main.php
@@ -157,7 +157,8 @@
 
 				<div class="buttonAlign">
 					<button type="button"" class="buttonStyle" id="migraineDataCancel" onclick="window.location='landing.html'">Cancel</button>
-					<button type="submit" class="buttonStyle" id="migraineDataSubmit" value="Add Migraine Data" onclick="window.location='home.html'">Submit</button>
+					<!--<button type="submit" class="buttonStyle" id="migraineDataSubmit" value="Add Migraine Data" onclick="window.location='submitCompMsg.html'">Submit</button>-->
+					<button type="submit" class="buttonStyle" id="migraineDataSubmit" value="submitCompMsg.html" formaction="submitCompMsg.html">Submit</button>
 				</div>
 
 			</form>
