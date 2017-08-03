e00852fd0c8dd012d91ef7fb6257202b4723718a
noemrd <noem@oregonstate.edu>
2017-08-03 08:33:01 -0400

    Delete DeleteRecords.sql

--- sql_ref/DeleteRecords.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM Migraine Where MigraineID = 17; 
-DELETE FROM HasFoodTriggers Where MigraineID = 17;
-DELETE FROM HasSensoryTriggers Where MigraineID = 17;
-DELETE FROM HasFoodTriggers Where MigraineID IS NULL;
-DELETE FROM HasSensoryTriggers Where MigraineID IS NULL;
