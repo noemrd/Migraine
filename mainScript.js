078a504b3029b35f3ec19693eb14539a690a894f
AL2016 <lamaa@oregonstate.edu>
2017-08-13 19:57:55 -0400

    mainform checkbox constraint

--- mainScript.js
+++ mainScript.js
@@ -15,7 +15,40 @@ function highlight(id1, id2){
  		$(id2).css("background-color", "yellow");
  }
 
+function checkCheckboxFood(){
+	var checkValues = document.getElementsByClassName("ftiCheckBox");
+	var checkStatus = false;
+	for(var i=0;i<7;i++){
+		if(checkValues[i].checked === true){
+			checkStatus = true;
+		}
+	}
+
+	if((checkStatus === true) && (checkValues[7].checked === true)){
+		alert("'None' check box cannot be checked alongside any of the 'Food and Drink Trigger' check boxes.");
+		return false;
+	}
+	return true;		
+}
+
+function checkCheckboxSensory(){
+	var checkValues = document.getElementsByClassName("sensoryCheckBox");
+	var checkStatus = false;
+	for(var i=0;i<5;i++){
+		if(checkValues[i].checked === true){
+			checkStatus = true;
+		}
+	}
+
+	if((checkStatus === true) && (checkValues[5].checked === true)){
+		alert("'None' check box cannot be checked alongside any of the 'Sensory Trigger' check boxes.");
+		return false;
+	}
+	return true;		
+}
+
 function compareDates() {     
+
 	if(!checkLength()){
 		return false;
 	}
@@ -65,4 +98,13 @@ function compareDates() {
          highlight("#MigraineEndTimestamp","#MigraineEndTimestamp");
          return false;
     }
+
+    if(!checkCheckboxFood()){
+		return false;
+	}
+
+	if(!checkCheckboxSensory()){
+		return false;
+	}
+
  }
